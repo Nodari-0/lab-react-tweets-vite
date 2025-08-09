52645847f54f2ad25c7b6e85ad0b988631e05056
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-08-09 16:16:49 +0200

    done and used the proptypes too

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,39 +1,86 @@
-function Tweet() {
-  return (
-    <div className="tweet">
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+// TweetList component displays a list of tweets and handles like functionality
+function TweetList({ tweets: initialTweets }) {
+  // Initialize state with tweets array, adding isLiked property to each tweet
+  const [tweets, setTweets] = useState(
+    initialTweets.map(tweet => ({
+      ...tweet,
+      isLiked: false, // Set initial like status to false
+    }))
+  );
+
+  // Handler function to toggle like status for a specific tweet
+  const handleToggleLike = (tweetIndex) => {
+    setTweets(prevTweets =>
+      prevTweets.map((tweet, index) =>
+        index === tweetIndex
+          ? { ...tweet, isLiked: !tweet.isLiked } // Toggle like status
+          : tweet
+      )
+    );
+  };
+
+  // Render list of tweets
+  return tweets.map((tweet, index) => (
+    // Individual tweet container
+    <div className="tweet" key={index}>
+      {/* User profile image */}
       <img
-        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/ih_logo.jpeg"
+        src={tweet.user.image}
+        alt={tweet.user.name}
         className="profile"
-        alt="profile"
       />
 
+      {/* Tweet content container */}
       <div className="body">
+        {/* User info and timestamp */}
         <div className="top">
           <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
+            <span className="name">{tweet.user.name}</span>
+            <span className="handle">{tweet.user.handle}</span>
           </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+          <span className="timestamp">{tweet.timestamp}</span>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        {/* Tweet message */}
+        <p className="message">{tweet.message}</p>
 
+        {/* Tweet action buttons */}
         <div className="actions">
-          {/* Font Awesome icons */}
           <i className="far fa-comment" data-testid="comment-icon"></i>
           <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
+          <i
+            className={`${tweet.isLiked ? 'fas' : 'far'} fa-heart`}
+            data-testid="heart-icon"
+            onClick={() => handleToggleLike(index)}
+            style={{ color: tweet.isLiked ? 'red' : 'inherit' }}
+          ></i>
           <i className="fas fa-share" data-testid="share-icon"></i>
         </div>
       </div>
 
+      {/* More options button */}
       <i className="fas fa-ellipsis-h"></i>
     </div>
-  );
+  ));
 }
 
-export default Tweet;
+// PropTypes validation for component props
+TweetList.propTypes = {
+  tweets: PropTypes.arrayOf(
+    PropTypes.shape({
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        handle: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+      }).isRequired,
+      timestamp: PropTypes.string.isRequired,
+      message: PropTypes.string.isRequired,
+      isLiked: PropTypes.bool,
+    })
+  ).isRequired,
+};
+
+export default TweetList;
